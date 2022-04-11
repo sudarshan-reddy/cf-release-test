31ff0e5b0dee324ecad75f104a0326f8b551e015
Sudarsan Reddy <sreddy@cloudflare.com>
2022-04-11 10:39:37 +0100

    sdded fileserver

--- functions/pkg.ts
+++ functions/pkg.ts
@@ -1,4 +1,18 @@
+var fs = require('fs');
 
 export const onRequest = () => {
-  return new Response(new Date().toISOString())
+    var filesLink="<ul>";
+    res.setHeader('Content-type', 'text/html');
+    var filesList=fs.readdirSync("./resources");
+      
+    filesList.forEach(element => {
+        if(fs.statSync("./"+element).isFile()) {
+            filesLink +=`<br/><li><a href='./${element}'>
+                ${element}
+            </a></li>` ;        
+        }
+    });
+    filesLink+="</ul>";
+     
+    res.end("<h1>List of files:</h1> " + filesLink);
 }
