e80f4603cf7446ae77cd68796f6908def20fe58e
Sudarsan Reddy <sreddy@cloudflare.com>
2022-04-11 13:59:20 +0100

    refactor ts

--- functions/pkg.ts
+++ functions/pkg.ts
@@ -1,16 +1,20 @@
 export const onRequest = () => {
-    var filesLink="<ul>";
+    var page = `<html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Directory listing for /</title>
+        </head>
+    <body>
+    <h1>Directory listing for /</h1>
+    <hr>
+    <ul>
+    <li><a href="https://github.com/sudarshan-reddy/cf-release-test/raw/main/debian/cloudflared-linux-amd64.deb">cloudflared-linux-amd64.deb</a></li>
+    <li><a href="https://github.com/sudarshan-reddy/cf-release-test/raw/main/debian/Packages.gz">Packages.gz</a></li>
+    </ul>
+    <hr>
+    </body>
+    </html>`
     res.setHeader('Content-type', 'text/html');
-    filesList = ["https://github.com/sudarshan-reddy/cf-release-test/blob/main/debian/Packages.gz"];
-      
-    filesList.forEach(element => {
-        if(fs.statSync("./"+element).isFile()) {
-            filesLink +=`<br/><li><a href='./${element}'>
-                ${element}
-            </a></li>` ;        
-        }
-    });
-    filesLink+="</ul>";
-     
-    res.end("<h1>List of files:</h1> " + filesLink);
+    res.end(page);
 }
+
