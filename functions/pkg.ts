13329cbeb498e3fbdb8aaa3f8c102c6a9cd3baa9
Sudarsan Reddy <sreddy@cloudflare.com>
2022-04-11 14:27:38 +0100

    typo

--- functions/pkg.ts
+++ functions/pkg.ts
@@ -14,5 +14,5 @@ export const onRequest = () => {
     <hr>
     </body>
     </html>`
-    return new Response(page, {headers: { "Content-Type": "text/html"}`)
+    return new Response(page, {headers: { "Content-Type": "text/html"})
 }
